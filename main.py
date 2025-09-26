1e550252ba493c4cd07874e6d6ce792ff79903b2
olino3101 <olivier_nault@hotmail.com>
2025-09-26 14:55:13 +0900

    yeah

--- main.py
+++ main.py
@@ -155,10 +155,10 @@ class MyAI(Alg3D):
 			# Example values : [-1, 1, 0, 2]
             values = [board[x][y][z] for (x,y,z) in line]
 			
-            if values.count(self.player) == 3 and values.count(0) == 1:
-                score += 100
-            elif values.count(self.player) == 2 and values.count(0) == 2:
-                score += 10
+            # if values.count(self.player) == 3 and values.count(0) == 1:
+            #     score += 100
+            # elif values.count(self.player) == 2 and values.count(0) == 2:
+            #     score += 10
 
             if values.count(enemy) == 3 and values.count(0) == 1:
                 score -= 100
